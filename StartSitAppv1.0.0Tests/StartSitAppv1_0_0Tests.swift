ae62cf1b3cd11fb8f86f70acce0afb28eefd16ff
Manny <jjimen50@calpoly.edu>
2019-03-03 18:40:22 -0800

    Initial Commit

---
+++ StartSitAppv1.0.0Tests/StartSitAppv1_0_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  StartSitAppv1_0_0Tests.swift
+//  StartSitAppv1.0.0Tests
+//
+//  Created by Juan Manuel Jimenez Ramos on 3/3/19.
+//  Copyright © 2019 Juan Manuel Jimenez Ramos. All rights reserved.
+//
+
+import XCTest
+@testable import StartSitAppv1_0_0
+
+class StartSitAppv1_0_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
